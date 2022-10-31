24976cf1aa5c57c2240b2d1b27316be6d73bfe0c
Stoia <alin58236@gmail.com>
2022-10-31 22:04:51 +0200

    Adds matplotlib and Pandas libraries for viewing data

---
+++ keys.py
@@ -0,0 +1,2 @@
+# Sign up for free account and get free key at https://detectlanguage.com/documentation
+deep_translator_key = '4e716625bb345516143661b9450ec377'
--- spellingandwords.py
+++ spellingandwords.py
@@ -4,7 +4,7 @@ from textblob import TextBlob  # for sentiment detection
 from deep_translator import GoogleTranslator  # for text translation
 
 GoogleTranslator().get_supported_languages(as_dict=True)  # language dictionary
-
+import keys  #api_key for translator api
 from deep_translator import single_detection  # detection for a single text
 
 #############
@@ -12,11 +12,9 @@ print("\n\n################################ SENTIMENT DETECTION ################
 #############
 
 
-text = 'Yesterday was a beautiful day. Tommorow looks like bad weather'
+text = "I am happy."
 
 blob = TextBlob(text)
-
-TextBlob('Yesterday was a beautiful day. Tommorow looks like bad weather')
 
 blob.sentiment  # sentiment object positive/negative and objective/subjective
 
@@ -27,19 +25,19 @@ for sentence in blob.sentences:
 print("\n\n################################ TRANSLATION #################################\n\n")
 #############
 
-blob= " Ce faci? "
+blob = " Ce faci? "
 
-single_detection(blob, api_key="4e716625bb345516143661b9450ec377")
+single_detection(blob, api_key=keys.deep_translator_key)
 
 spanish = GoogleTranslator(source='auto', target='es').translate(blob)
 
 print(spanish)
 
-#chinese = GoogleTranslator(source='auto', target='zh-CN').translate(blob)
-#print(chinese)
+  # chinese = GoogleTranslator(source='auto', target='zh-CN').translate(blob)
+  # print(chinese)
 
-#english = GoogleTranslator(source='auto', target='en').translate(blob)
-#print(english)
+  #  english = GoogleTranslator(source='auto', target='en').translate(blob)
+  #  print(english)
 
 
 #############
@@ -47,8 +45,6 @@ print("\n\n################################# TRANSLATION 2 #####################
 #############
 
 
-
-blob
 # autodetect source language and translate to English
 english=GoogleTranslator(source='auto', target='en').translate(spanish)
 print(english)
@@ -82,3 +78,79 @@ print(sentence)
 
 print(sentence.correct())
 
+
+#############
+print("\n\n################################# Big Text Processing ################################\n\n")
+#############
+
+
+
+from pathlib import Path
+
+blob = TextBlob(Path('PythonDataScienceFullThrottle/ch11/1513-0_RomeoAndJulietOriginalDownload.txt').read_text())   # load romeo and juliet book
+
+print(blob.word_counts['romeo'])  # counts 'romeo' instances
+
+
+#############
+print("\n\n################################# Deleting Stop Words ################################\n\n")
+#############
+
+import nltk
+#nltk.download('stopwords')  # must download before first use
+from nltk.corpus import stopwords
+
+stops = stopwords.words('english')
+stops
+blob = TextBlob('These flowers are so beautiful!')
+# keep anything that's not a stop word
+[word for word in blob.words.lower() if word not in stops]
+print(blob)
+
+#############
+print("\n\n################################# Visualizing Word Frequencies With Bar Charts and Word Clouds ################################\n\n")
+#############
+
+
+
+blob = TextBlob(Path('PythonDataScienceFullThrottle/ch11/1513-0_RomeoAndJulietOriginalDownload.txt').read_text())  #load book
+
+items = blob.word_counts.items()  #iterator for word-frequency
+
+items = [item for item in items if item[0] not in stops and item[0] != "'"]
+
+print(items[:10])
+
+#############
+print("\n\n################################# Sorting the top 20 Words in descending order ################################\n\n")
+#############
+
+from operator import itemgetter
+
+sorted_items = sorted(items, key=itemgetter(1), reverse=True)  # descending
+
+# key=itemgetter(1) - sort tuples by frequency (each tuple's element = 1)
+
+top20 = sorted_items[0:20]
+
+print(top20)
+
+
+
+#############
+print("\n\n################################# Put Top20 into dataFrame for vizualization ################################\n\n")
+#############
+
+from IPython.display import display
+import pandas as pd
+
+df = pd.DataFrame(top20)
+
+display(df)
+
+import matplotlib.pyplot as plt
+
+axes= df.plot.bar(x='word', y='count')
+plt.gcf().tight_layout()
+
+plt.show(x,y)
